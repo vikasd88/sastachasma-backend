ce7d4f6e4eaf0a835c4aee9df8cd600d7db8d297
Vikas <65715532+vikasd88@users.noreply.github.com>
2025-11-12 21:52:17 +0530

    ms split

---
+++ api-gateway/src/main/java/com/sastachasma/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,31 @@
+package com.sastachasma.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:4200"); // Allow your Angular frontend origin
+        corsConfig.addAllowedMethod("*"); // Allow all HTTP methods
+        corsConfig.addAllowedHeader("*"); // Allow all headers
+        corsConfig.setAllowCredentials(true); // Allow credentials (e.g., cookies, authorization headers)
+        corsConfig.setMaxAge(3600L); // Cache preflight response for 1 hour
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig); // Apply CORS to all paths
+        return new CorsWebFilter(source);
+    }
+}
--- product-service/src/main/java/com/sastachasma/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/sastachasma/product/ProductServiceApplication.java
@@ -3,15 +3,15 @@ package com.sastachasma.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EntityScan("com.sastachasma.product.entity")
 @EnableJpaRepositories("com.sastachasma.product.repository")
-@ComponentScan("com.sastachasma.product")
+@EnableJpaAuditing
 public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
